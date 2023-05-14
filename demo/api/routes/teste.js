a4020f13b9ef8aa314a2b486b90fd135b9fa4eec
webapisbr <133459926+webapisbr@users.noreply.github.com>
2023-05-14 00:34:16 -0300

    Delete demo directory

--- demo/api/routes/teste.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('', async function(req, res, next) {
-        res.statusCode = 200;
-        res.json({results: 'teste'});
-});
-
-module.exports = router;
