bab1e2f5bb667f3e88e3d2b52ffc1bce9d4e4fa8
webapisbr <133459926+webapisbr@users.noreply.github.com>
2023-05-14 00:44:07 -0300

    Create indes.js

---
+++ indes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const testeRouter = require('./routes/teste');
+//const instagramRouter = require('./routes/instagram');
+
+app.use('/teste', testeRouter);
+//app.use('/instagram', instagramRouter);
+
+app.use(function(req, res, next) {
+    res.statusCode = 404;
+    res.json({message: 'Rota inválida'});
+});
+
+app.listen(process.env.PORT||3000);
+
+exports.handler = app;
